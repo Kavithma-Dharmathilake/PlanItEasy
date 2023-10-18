dbf87e54432963c0c8210489f0204af6408b4c7e
Kavithma Dharmathilake <117156528+Kavithma-Dharmathilake@users.noreply.github.com>
2023-10-18 14:53:47 +0530

    all packages

---
+++ app/views/eventplanners/packages.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Packages</title>
+    <!-- MATERIAL CDN -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <!-- STYLESHEET -->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>public/css/admindash.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>public/css/eventplannerdash.css">
+
+
+</head>
+
+<body>
+    <div class="dash-container">
+        <aside>
+            <div class="top">
+                <div class="logo">
+                    <img src="<?php echo URLROOT; ?>/public/images/logo.jpg">
+                    <h2>PlanItEasy</h2>
+                </div>
+                <div class="close" id="close-btn">
+                    <span class="material-icons-sharp">close</span>
+                </div>
+            </div>
+            <div class="sidebar">
+                <a href="<?php echo URLROOT; ?>eventplanners">
+                    <span class="material-icons-sharp">grid_view</span>
+                    <h3>Dashboard</h3>
+                </a>
+
+                <a href="<?php echo URLROOT ?>eventplanners/packages" class="active">
+                    <span class="material-icons-sharp">
+                        inventory
+                    </span>
+                    <h3>Packages</h3>
+                </a>
+
+                <a href="<?php echo URLROOT ?>eventplanners/eventRequest">
+                    <span class="material-icons-sharp">
+                        festival
+                    </span>
+                    <h3>Event Requests</h3>
+                </a>
+
+                <a href="<?php echo URLROOT ?>eventplanners/quoteReq">
+                    <span class="material-icons-sharp">
+                        note_add
+                    </span>
+                    <h3>Quoatation Requests</h3>
+                </a>
+                <a href="<?php echo URLROOT ?>eventplanners/supplierReq">
+                    <span class="material-icons-sharp">
+                        request_quote
+                    </span>
+                    <h3>Supplier Quotations</h3>
+                </a>
+                <a href="<?php echo URLROOT ?>eventplanners/budget">
+                    <span class="material-icons-sharp">
+                        paid
+                    </span>
+                    <h3>Budget Plans</h3>
+                </a>
+
+                <a href="<?php echo URLROOT ?>eventplanners/calendar">
+                    <span class="material-icons-sharp">
+                        calendar_month
+                    </span>
+                    <h3>Calender</h3>
+                </a>
+
+                <a href="<?php echo URLROOT ?>eventplanners/profile">
+                    <span class="material-icons-sharp">
+                        account_box
+                    </span>
+                    <h3>Profile</h3>
+                </a>
+
+                <a href="<?php echo URLROOT ?>eventplanners/messages">
+                    <span class="material-icons-sharp">mail</span>
+                    <h3>Messages</h3>
+                    <span class="message-count">26</span>
+                </a>
+
+                <a href="<?php echo URLROOT ?>eventplanners/inquiry">
+                    <span class="material-icons-sharp">
+                        info
+                    </span>
+                    <h3>Inquiry</h3>
+                </a>
+                <a href="<?php echo URLROOT ?>">
+                    <span class="material-icons-sharp">logout</span>
+                    <h3>Logout</h3>
+                </a>
+            </div>
+        </aside>
+
+
+        <!-- Content start here -->
+        <div>
+            <div class="profile end">
+                <div class="info" style="padding-right:25px;">
+                    <p>Hey, <b>Sunimal</b></p>
+                    <small class="text-muted">Eventplanner</small>
+                </div>
+                <div class="profile-photo">
+                    <img src="<?php echo URLROOT ?>public/images/photo2.jpg">
+                </div>
+            </div>
+
+
+
+            <!-- Heading and search bar -->
+            <div style="display:flex">
+
+                <div class="planner-title">
+                    <h1>Packages and services</h1>
+
+
+                </div>
+                <div class="planner-search">
+                    <form action="#" method="post">
+
+                        <input type="search" id="query" name="q" placeholder="       Search Packages"
+                            class="planner-textbox">
+
+                        <button>
+                            <i class="fa fa-search" style="font-size: 18px;">
+                            </i>
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+            <!-- Add button -->
+            <div style="padding-top:40px;">
+                <button class="package-add">
+                    <i class="fa fa-add" style="font-size: 18px; padding-right:15px;">
+                    </i>
+                    Add new Package
+                </button>
+            </div>
+
+            <!-- List of packages -->
+            <!--  2 packages at one side-->
+            <div class="planner-packages">
+
+                <a href="">
+                    <div class="planner-package">
+                        <div class="package-title">
+                            <h3>Wedding package</h3>
+                        </div>
+                        <div class="package-content">
+                            <img src="<?php echo URLROOT ?>public/images/photo2.jpg" alt="package-image">
+                            <p>
+                                package description
+                            </p>
+                        </div>
+                        <div class="package-action">
+                            <strong>RS.XXXX.XX</strong>
+
+                            <div class="button-left">
+                                <a href=""><button class="package-edit">
+                                        <i class="fa fa-edit" style="font-size: 18px; padding-right:15px;">
+                                        </i>
+                                        Edit</button></a>
+                                <a href=""><button class="package-delete">
+                                        <i class="fa fa-trash" style="font-size: 18px; padding-right:15px;">
+                                        </i>
+
+                                        Delete</button></a>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </a>
+                <a href="">
+                    <div class="planner-package">
+                        <div class="package-title">
+                            <h3>Wedding package</h3>
+                        </div>
+                        <div class="package-content">
+                            <img src="<?php echo URLROOT ?>public/images/photo2.jpg" alt="package-image">
+                            <p>
+                                package description
+                            </p>
+                        </div>
+                        <div class="package-action">
+                            <strong>RS.XXXX.XX</strong>
+
+                            <div class="button-left">
+                                <a href=""><button class="package-edit">
+                                        <i class="fa fa-edit" style="font-size: 18px; padding-right:15px;">
+                                        </i>
+                                        Edit</button></a>
+                                <a href=""><button class="package-delete">
+                                        <i class="fa fa-trash" style="font-size: 18px; padding-right:15px;">
+                                        </i>
+
+                                        Delete</button></a>
+                            </div>
+
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="planner-packages">
+                <div class="planner-package">
+                    <div class="package-title">
+                        <h3>Wedding package</h3>
+                    </div>
+                    <div class="package-content">
+                        <img src="<?php echo URLROOT ?>public/images/photo2.jpg" alt="package-image">
+                        <p>
+                            package description
+                        </p>
+                    </div>
+                    <div class="package-action">
+                        <strong>RS.XXXX.XX</strong>
+
+                        <div class="button-left">
+                            <a href=""><button class="package-edit">
+                                    <i class="fa fa-edit" style="font-size: 18px; padding-right:15px;">
+                                    </i>
+                                    Edit</button></a>
+                            <a href=""><button class="package-delete">
+                                    <i class="fa fa-trash" style="font-size: 18px; padding-right:15px;">
+                                    </i>
+
+                                    Delete</button></a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="planner-package">
+                    <div class="package-title">
+                        <h3>Wedding package</h3>
+                    </div>
+                    <div class="package-content">
+                        <img src="<?php echo URLROOT ?>public/images/photo2.jpg" alt="package-image">
+                        <p>
+                            package description
+                        </p>
+                    </div>
+                    <div class="package-action">
+                        <strong>RS.XXXX.XX</strong>
+
+                        <div class="button-left">
+                            <a href=""><button class="package-edit">
+                                    <i class="fa fa-edit" style="font-size: 18px; padding-right:15px;">
+                                    </i>
+                                    Edit</button></a>
+                            <a href=""><button class="package-delete">
+                                    <i class="fa fa-trash" style="font-size: 18px; padding-right:15px;">
+                                    </i>
+
+                                    Delete</button></a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+
+</body>
+
+</html>
