a4cca260c5ab4bf0c23cf899dea0e2b3d3534f83
Kavithma Dharmathilake <117156528+Kavithma-Dharmathilake@users.noreply.github.com>
2024-04-21 17:25:42 +0530

    stripe integrated

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -8,6 +8,9 @@ require_once 'config/config.php';
 //Load helpers
 require_once 'helpers/url_helper.php';
 require_once 'helpers/session_helper.php';
+require_once 'vendor/autoload.php';
+
+
 
 //Autoload core Libraries
 spl_autoload_register(function($className){
